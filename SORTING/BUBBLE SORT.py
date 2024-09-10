d35903dad82cb52c7280a437606e552438e698ca
janani asokan <chicatechhub@gmail.com>
2024-09-10 07:34:04 +0530

    BUBBLE SORT.py

---
+++ SORTING/BUBBLE SORT.py
@@ -0,0 +1,9 @@
+Example 1:
+Input: N = 6, array[] = {13,46,24,52,20,9}
+Output: 9,13,20,24,46,52
+Explanation: After sorting we get 9,13,20,24,46,52
+
+
+Input: N = 5, array[] = {5,4,3,2,1}
+Output: 1,2,3,4,5
+Explanation: After sorting we get 1,2,3,4,5
