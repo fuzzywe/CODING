97f72d7c99922cb18f31e9eb1967f85ba78e23c3
janani asokan <chicatechhub@gmail.com>
2024-09-17 06:21:48 +0530

    Create Value equal to index value.cpp

---
+++ SORTING/Value equal to index value.cpp
@@ -0,0 +1,9 @@
+Input: arr[] = [15, 2, 45, 4 , 7]
+Output: 2 , 4
+Explanation: 
+Here, arr[2] = 2 exists here.
+and arr[4] = 4 exists here.
+Input: arr[] = [1]
+Output: 1
+Explanation: Here arr[1] = 1 exists.
+
