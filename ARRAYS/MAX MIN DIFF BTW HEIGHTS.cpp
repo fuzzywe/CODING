1c556e5489d39570d3678384f183728957aa39d1
maha <chicatechhub@gmail.com>
2024-09-22 10:15:39 +0530

    Create MAX MIN DIFF BTW HEIGHTS.cpp

---
+++ ARRAYS/MAX MIN DIFF BTW HEIGHTS.cpp
@@ -0,0 +1,84 @@
+Minimize the maximum difference between the heights
+Last Updated : 26 Jun, 2023
+Given the heights of N towers and a value of K, Either increase or decrease the height of every tower by K (only once) where K > 0. After modifications, the task is to minimize the difference between the heights of the longest and the shortest tower and output its difference.
+
+Examples: 
+
+Input: arr[] = {1, 15, 10}, k = 6
+Output:  Maximum difference is 5.
+Explanation: Change 1 to 7, 15 to 9 and 10 to 4. Maximum difference is 5 (between 4 and 9). We can’t get a lower difference.
+
+Input: arr[] = {1, 5, 15, 10}, k = 3   
+Output: Maximum difference is 8, arr[] = {4, 8, 12, 7}
+
+
+//{ Driver Code Starts
+// Initial template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+// User function template for C++
+
+class Solution {
+  public:
+    int getMinDiff(vector<int> &arr, int k) {
+        // code here
+        int n = arr.size();
+        sort(arr.begin(), arr.end());
+ 
+    // Maximum possible height difference
+    int ans = arr[n - 1] - arr[0];
+ 
+    int tempmin, tempmax;
+    tempmin = arr[0];
+    tempmax = arr[n - 1];
+ 
+    for (int i = 1; i < n; i++) {
+ 
+        // If on subtracting k we got
+        // negative then continue
+        if (arr[i] - k < 0)
+            continue;
+ 
+        // Minimum element when we
+        // add k to whole array
+        tempmin = min(arr[0] + k, arr[i] - k);
+ 
+        // Maximum element when we
+        // subtract k from whole array
+        tempmax = max(arr[i - 1] + k, arr[n - 1] - k);
+ 
+        ans = min(ans, tempmax - tempmin);
+    }
+    return ans;
+    }
+};
+
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    cin.ignore();
+    while (t--) {
+        int n, k;
+        cin >> k;
+        cin.ignore();
+        vector<int> a, b, c, d;
+        string input;
+        getline(cin, input);
+        stringstream ss(input);
+        int num;
+        while (ss >> num)
+            a.push_back(num);
+
+        Solution ob;
+        auto ans = ob.getMinDiff(a, k);
+        cout << ans << "\n";
+    }
+    return 0;
+}
+// } Driver Code Ends
