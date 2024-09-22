1695b6f274137f7180035611941f968601cf60af
maha <chicatechhub@gmail.com>
2024-09-22 10:00:07 +0530

    Create kADANES ALGO.cpp

---
+++ ARRAYS/kADANES ALGO.cpp
@@ -0,0 +1,126 @@
+Kadane's Algorithm
+Difficulty: MediumAccuracy: 36.28%Submissions: 979K+Points: 4
+Given an integer array arr[]. Find the contiguous sub-array(containing at least one number) that has the maximum sum and return its sum.
+
+Examples:
+
+Input: arr[] = [1, 2, 3, -2, 5]
+Output: 9
+Explanation: Max subarray sum is 9 of elements (1, 2, 3, -2, 5) which is a contiguous subarray.
+Input: arr[] = [-1, -2, -3, -4]
+Output: -1
+Explanation: Max subarray sum is -1 of element (-1)
+Input: arr[] = [5, 4, 7]
+Output: 16
+Explanation: Max subarray sum is 16 of element (5, 4, 7)
+Expected Time Complexity: O(n)
+Expected Auxiliary Space: O(1)
+
+  Example 1:
+Input:
+ arr = [-2,1,-3,4,-1,2,1,-5,4] 
+
+Output:
+ 6 
+
+Explanation:
+ [4,-1,2,1] has the largest sum = 6. 
+
+Examples 2:
+Input:
+ arr = [1] 
+
+Output:
+ 1 
+
+Explanation:
+ Array has only one element and which is giving positive sum of 1.
+
+
+  
+
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int maxSubarraySum(int arr[], int n) {
+    int maxi = INT_MIN; // maximum sum
+
+    for (int i = 0; i < n; i++) {
+        int sum = 0;
+        for (int j = i; j < n; j++) {
+            // current subarray = arr[i.....j]
+
+            //add the current element arr[j]
+            // to the sum i.e. sum of arr[i...j-1]
+            sum += arr[j];
+
+            maxi = max(maxi, sum); // getting the maximum
+        }
+    }
+
+    return maxi;
+}
+
+int main()
+{
+    int arr[] = { -2, 1, -3, 4, -1, 2, 1, -5, 4};
+    int n = sizeof(arr) / sizeof(arr[0]);
+    int maxSum = maxSubarraySum(arr, n);
+    cout << "The maximum subarray sum is: " << maxSum << endl;
+    return 0;
+}
+
+Complexity Analysis
+Time Complexity: O(N2), where N = size of the array.
+Reason: We are using two nested loops, each running approximately N times.
+
+Space Complexity: O(1) as we are not using any extra space.
+
+
+  
+#include <bits/stdc++.h>
+using namespace std;
+
+long long maxSubarraySum(int arr[], int n) {
+    long long maxi = LONG_MIN; // maximum sum
+    long long sum = 0;
+
+    for (int i = 0; i < n; i++) {
+
+        sum += arr[i];
+
+        if (sum > maxi) {
+            maxi = sum;
+        }
+
+        // If sum < 0: discard the sum calculated
+        if (sum < 0) {
+            sum = 0;
+        }
+    }
+
+    // To consider the sum of the empty subarray
+    // uncomment the following check:
+
+    //if (maxi < 0) maxi = 0;
+
+    return maxi;
+}
+
+int main()
+{
+    int arr[] = { -2, 1, -3, 4, -1, 2, 1, -5, 4};
+    int n = sizeof(arr) / sizeof(arr[0]);
+    long long maxSum = maxSubarraySum(arr, n);
+    cout << "The maximum subarray sum is: " << maxSum << endl;
+    return 0;
+}
+
+Output: The maximum subarray sum is: 6
+
+Complexity Analysis
+Time Complexity: O(N), where N = size of the array.
+Reason: We are using a single loop running N times.
+
+Space Complexity: O(1) as we are not using any extra space.
