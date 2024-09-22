9270b53114edcb64d60613483e79c92a3585822e
maha <chicatechhub@gmail.com>
2024-09-22 09:12:34 +0530

    Create Rotate Array by One.cpp

---
+++ ARRAYS/Rotate Array by One.cpp
@@ -0,0 +1,69 @@
+Rotate Array by One
+
+Examples:
+
+Input: arr = [1, 2, 3, 4, 5]
+Output: [5, 1, 2, 3, 4]
+Explanation: If we rotate arr by one position in clockwise 5 come to the front and remaining those are shifted to the end.
+Input: arr = [9, 8, 7, 6, 4, 2, 1, 3]
+Output: [3, 9, 8, 7, 6, 4, 2, 1]
+Explanation: After rotating clock-wise 3 comes in first position.
+Expected Time Complexity: O(n)
+Expected Auxiliary Space: O(1)
+
+  #include<bits/stdc++.h>
+
+using namespace std;
+void solve(int arr[], int n) {
+  int temp[n];
+  for (int i = 1; i < n; i++) {
+    temp[i - 1] = arr[i];
+  }
+  temp[n - 1] = arr[0];
+  for (int i = 0; i < n; i++) {
+    cout << temp[i] << " ";
+  }
+  cout << endl;
+}
+int main() {
+  int n=5;
+  
+  int arr[]= {1,2,3,4,5};
+  solve(arr, n);
+}
+
+Output: 2 3 4 5 1
+
+Complexity Analysis
+Time Complexity: O(n), as we iterate through the array only once.
+
+Space Complexity: O(n) as we are using another array of size, same as the given array.
+
+
+  #include<bits/stdc++.h>
+
+using namespace std;
+void solve(int arr[], int n) {
+  int temp = arr[0]; // storing the first element of array in a variable
+  for (int i = 0; i < n - 1; i++) {
+    arr[i] = arr[i + 1];
+  }
+  arr[n - 1] = temp; // assigned the value of variable at the last index
+  for (int i = 0; i < n; i++) {
+    cout << arr[i] << " ";
+  }
+
+}
+int main() {
+  int n=5;
+  
+  int arr[]= {1,2,3,4,5};
+  solve(arr, n);
+}
+
+Output: 2 3 4 5 1
+
+Complexity Analysis
+Time Complexity: O(n), as we iterate through the array only once.
+
+Space Complexity: O(1) as no extra space is used
