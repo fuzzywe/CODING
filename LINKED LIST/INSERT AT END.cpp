c2ec2ddc766af59fc7554f39a33b83b307a2967a
maha <chicatechhub@gmail.com>
2024-09-28 10:38:31 +0530

    Update INSERT AT END.cpp

--- LINKED LIST/INSERT AT END.cpp
+++ LINKED LIST/INSERT AT END.cpp
@@ -1,10 +1,6 @@
 Insert at the head of a Linked List
 
-
-68
-
-2
-Problem Statement:  Given a linked list and an integer value val, insert a new node with that value at the beginning (before the head) of the list and return the updated linked list.
+https://www.geeksforgeeks.org/problems/linked-list-insertion-1587115620/0?utm_source=youtube&utm_medium=collab_striver_ytdescription&utm_campaign=linked-list-insertion
 
 Examples
 Example 1:
@@ -179,6 +175,235 @@ class Solution {
         return head;
     }
 };
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    cin.ignore();
+
+    while (t--) {
+        vector<int> arr;
+        string input;
+        getline(cin, input);
+        stringstream ss(input);
+        int number;
+
+        // Read numbers from the input line
+        while (ss >> number) {
+            arr.push_back(number);
+        }
+
+        int x;
+        cin >> x;
+        cin.ignore();
+
+        Node* head = nullptr;
+
+        // Check if the array is empty
+        if (!arr.empty()) {
+            head = new Node(arr[0]);
+            Node* tail = head;
+            for (size_t i = 1; i < arr.size(); ++i) {
+                tail->next = new Node(arr[i]);
+                tail = tail->next;
+            }
+        }
+
+        Solution ob;
+        Node* ans = ob.insertAtEnd(head, x);
+        printList(ans);
+    }
+
+    return 0;
+}
+// } Driver Code Ends
+
+THE MISTAKE I DID:
+
+//{ Driver Code Starts
+// Initial Template for C++
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Node {
+    int data;
+    struct Node* next;
+
+    Node(int x) {
+        data = x;
+        next = NULL;
+    }
+};
+
+void printList(Node* node) {
+    while (node != NULL) {
+        cout << node->data << " ";
+        node = node->next;
+    }
+    cout << "\n";
+}
+
+
+// } Driver Code Ends
+/*Structure of the linked list node is as
+struct Node {
+  int data;
+  struct Node * next;
+  Node(int x) {
+    data = x;
+    next = NULL;
+  }
+}; */
+struct Node{
+int data;
+        struct Node* next;
+        
+        Node(int data1){
+            data = data1;
+            next = NULL;
+        }};
+        
+        void printList(Node* head){
+            while(head != NULL){
+                cout<<head->data<<" ";
+                head =head->next;
+            }
+        }
+
+class Solution {
+  public:
+  
+    Node *insertAtEnd(Node *head, int x) {
+        // Code here
+        Node* temp = new Node(x);
+        if (head==NULL){
+        return temp;}
+        Node* last = head;
+        while(last->next != NULL){
+            last =last ->next;
+        }
+        last->next = temp;
+        return head;
+    }
+};
+
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    cin.ignore();
+
+    while (t--) {
+        vector<int> arr;
+        string input;
+        getline(cin, input);
+        stringstream ss(input);
+        int number;
+
+        // Read numbers from the input line
+        while (ss >> number) {
+            arr.push_back(number);
+        }
+
+        int x;
+        cin >> x;
+        cin.ignore();
+
+        Node* head = nullptr;
+
+        // Check if the array is empty
+        if (!arr.empty()) {
+            head = new Node(arr[0]);
+            Node* tail = head;
+            for (size_t i = 1; i < arr.size(); ++i) {
+                tail->next = new Node(arr[i]);
+                tail = tail->next;
+            }
+        }
+
+        Solution ob;
+        Node* ans = ob.insertAtEnd(head, x);
+        printList(ans);
+    }
+
+    return 0;
+}
+// } Driver Code Ends./Solution.cpp:35:8: error: redefinition of 'struct Node'
+ struct Node{
+        ^
+./Solution.cpp:6:8: error: previous definition of 'struct Node'
+ struct Node {
+        ^
+./Solution.cpp: In function 'void printList(Node*)':
+./Solution.cpp:44:14: error: redefinition of 'void printList(Node*)'
+         void printList(Node* head){
+              ^
+./Solution.cpp:16:6: note: 'void printList(Node*)' previously defined here
+ void printList(Node* node) {
+      ^
+
+Expected Output: 
+
+
+THE SOLUTION:
+
+//{ Driver Code Starts
+// Initial Template for C++
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Node {
+    int data;
+    struct Node* next;
+
+    Node(int x) {
+        data = x;
+        next = NULL;
+    }
+};
+
+void printList(Node* node) {
+    while (node != NULL) {
+        cout << node->data << " ";
+        node = node->next;
+    }
+    cout << "\n";
+}
+
+
+// } Driver Code Ends
+/*Structure of the linked list node is as
+struct Node {
+  int data;
+  struct Node * next;
+  Node(int x) {
+    data = x;
+    next = NULL;
+  }
+}; */
+     
+        
+class Solution {
+  public:
+  
+    Node *insertAtEnd(Node *head, int x) {
+        // Code here
+        Node* temp = new Node(x);
+        if (head==NULL){
+        return temp;}
+        Node* last = head;
+        while(last->next != NULL){
+            last =last ->next;
+        }
+        last->next = temp;
+        return head;
+    }
+};
+
 
 //{ Driver Code Starts.
 
