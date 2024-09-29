80ffd43cc64f1a6ed5a2747355e1a28c5ab20a93
JANANI ASOKAN <chicatechhub@gmail.com>
2024-09-29 18:49:21 +0530

    Create REMOVE LOOP.cpp

---
+++ LINKED LIST/REMOVE LOOP.cpp
@@ -0,0 +1,222 @@
+Remove loop in Linked List
+Difficulty: MediumAccuracy: 27.66%Submissions: 467K+Points: 4
+Given the head of a linked list that may contain a loop.  A loop means that the last node of the linked list is connected back to a node in the same list.  So if the next of the previous node is null. then there is no loop.  Remove the loop from the linked list, if it is present (we mainly need to make the next of the last node null). Otherwise, keep the linked list as it is.
+
+Note: Given an integer, pos (1 based index)  Position of the node to which the last node links back if there is a loop. If the linked list does not have any loop, then pos = 0.
+
+The generated output will be true if your submitted code is correct, otherwise, false.
+
+Examples:
+
+Input: Linked list: 1->3->4, pos = 2
+Output: true
+Explanation: The linked list looks like
+
+A loop is present. If you remove it successfully, the answer will be true. 
+Input: Linked list: 1->8->3->4, pos = 0
+Output: true
+Explanation: 
+
+The Linked list does not contains any loop. 
+Input: Linked list: 1->2->3->4, pos = 1
+Output: true
+Explanation: The linked list looks like 
+
+A loop is present. If you remove it successfully, the answer will be true. 
+Expected Time Complexity: O(n)
+Expected Space Complexity: O(1)
+
+
+
+  BRUTE FORCE:
+
+class Solution {
+  public:
+    // Function to remove a loop in the linked list.
+    void removeLoop(Node* head) {
+        // code here
+        // just remove the loop without losing any nodes
+        Node* prev = nullptr;
+        unordered_set<Node*>st;
+        while(head!=NULL)
+        {
+            if(st.find(head) == st.end())
+            {
+                st.insert(head);
+                prev = head;
+                head = head->next;
+            }
+            else{
+                prev->next = nullptr;
+                break;
+                
+            }
+        }
+    }
+};
+
+FULL SOLUTION:
+
+//{ Driver Code Starts
+// driver code
+
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Node {
+    int data;
+    Node* next;
+
+    Node(int val) {
+        data = val;
+        next = NULL;
+    }
+};
+
+void loopHere(Node* head, Node* tail, int position) {
+    if (position == 0)
+        return;
+
+    Node* walk = head;
+    for (int i = 1; i < position; i++)
+        walk = walk->next;
+    tail->next = walk;
+}
+
+bool isLoop(Node* head) {
+    if (!head)
+        return false;
+
+    Node* fast = head->next;
+    Node* slow = head;
+
+    while (fast != slow) {
+        if (!fast || !fast->next)
+            return false;
+        fast = fast->next->next;
+        slow = slow->next;
+    }
+
+    return true;
+}
+
+int length(Node* head) {
+    int ret = 0;
+    while (head) {
+        ret++;
+        head = head->next;
+    }
+    return ret;
+}
+
+bool notOriginal(Node* head, unordered_map<Node*, int>& myMap) {
+
+    while (head) {
+        if (myMap.find(head) == myMap.end())
+            return true;
+        if (myMap[head] != (head->data))
+            return true;
+
+        head = head->next;
+    }
+}
+
+
+// } Driver Code Ends
+/*
+structure of linked list node:
+
+struct Node
+{
+    int data;
+    Node* next;
+
+    Node(int val)
+    {
+        data = val;
+        next = NULL;
+    }
+};
+
+*/
+
+class Solution {
+  public:
+    // Function to remove a loop in the linked list.
+    void removeLoop(Node* head) {
+        // code here
+        // just remove the loop without losing any nodes
+        Node* prev = nullptr;
+        unordered_set<Node*>st;
+        while(head!=NULL)
+        {
+            if(st.find(head) == st.end())
+            {
+                st.insert(head);
+                prev = head;
+                head = head->next;
+            }
+            else{
+                prev->next = nullptr;
+                break;
+                
+            }
+        }
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    cin.ignore();
+    while (t--) {
+        vector<int> arr;
+        string input;
+        getline(cin, input);
+        stringstream ss(input);
+        int number;
+        while (ss >> number) {
+            arr.push_back(number);
+        }
+
+        unordered_map<Node*, int> myMap;
+
+        int n, num;
+        n = arr.size();
+
+        Node *head, *tail;
+        num = arr[0];
+        head = tail = new Node(num);
+
+        myMap[head] = num;
+
+        for (int i = 1; i < n; i++) {
+            num = arr[i];
+            tail->next = new Node(num);
+            tail = tail->next;
+            myMap[tail] = num;
+        }
+
+        int pos;
+        cin >> pos;
+        cin.ignore();
+        loopHere(head, tail, pos);
+
+        Solution ob;
+        ob.removeLoop(head);
+
+        if (isLoop(head) || length(head) != n || notOriginal(head, myMap))
+            cout << "false\n";
+        else
+            cout << "true\n";
+    }
+    return 0;
+}
+
+// } Driver Code Ends
+
+
+OPTIMIZED SOLUTION:
+
