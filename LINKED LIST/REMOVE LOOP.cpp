fc8c6f71d55717a9ae7df50444eec0428797145e
JANANI ASOKAN <chicatechhub@gmail.com>
2024-09-29 18:53:12 +0530

    Update REMOVE LOOP.cpp

--- LINKED LIST/REMOVE LOOP.cpp
+++ LINKED LIST/REMOVE LOOP.cpp
@@ -220,3 +220,129 @@ int main() {
 
 OPTIMIZED SOLUTION:
 
+void detectAndRemoveLoop(Node *head) {
+  
+    // If list is empty or has only one node without loop
+    if (head == nullptr || head->next ==nullptr)
+        return;
+
+    Node *slow = head, *fast = head;
+
+    // Move slow and fast 1 and 2 steps ahead respectively.
+    slow = slow->next;
+    fast = fast->next->next;
+
+    // Search for loop using slow and fast pointers
+    while (fast && fast->next) {
+        if (slow == fast)
+            break;
+        slow = slow->next;
+        fast = fast->next->next;
+    }
+
+    // If loop exists
+    if (slow == fast) {
+        slow = head;
+
+        // this check is needed when slow and fast both meet
+        // at the head of the LL eg: 1->2->3->4->5 and then
+        // 5->next = 1 i.e the head of the LL
+        if (slow == fast)
+            while (fast->next != slow)
+                fast = fast->next;
+        else {
+            while (slow->next != fast->next) {
+                slow = slow->next;
+                fast = fast->next;
+            }
+        }
+
+        // since fast->next is the looping point 
+        fast->next = nullptr;
+    }
+}
+
+
+FULL SOLUTION
+
+// C++ program Using Floyd's Cycle Detection Algorithm
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Node {
+    int data;
+    Node *next;
+
+    Node(int x) {
+        data = x;
+        next = nullptr;
+    }
+};
+
+void printList(Node *curr) {
+    while (curr != nullptr) {
+        cout << curr->data << " ";
+        curr = curr->next;
+    }
+    cout << endl;
+}
+
+// Function to detect and remove loop in a linked list that
+// may contain loop
+void detectAndRemoveLoop(Node *head) {
+  
+    // If list is empty or has only one node without loop
+    if (head == nullptr || head->next ==nullptr)
+        return;
+
+    Node *slow = head, *fast = head;
+
+    // Move slow and fast 1 and 2 steps ahead respectively.
+    slow = slow->next;
+    fast = fast->next->next;
+
+    // Search for loop using slow and fast pointers
+    while (fast && fast->next) {
+        if (slow == fast)
+            break;
+        slow = slow->next;
+        fast = fast->next->next;
+    }
+
+    // If loop exists
+    if (slow == fast) {
+        slow = head;
+
+        // this check is needed when slow and fast both meet
+        // at the head of the LL eg: 1->2->3->4->5 and then
+        // 5->next = 1 i.e the head of the LL
+        if (slow == fast)
+            while (fast->next != slow)
+                fast = fast->next;
+        else {
+            while (slow->next != fast->next) {
+                slow = slow->next;
+                fast = fast->next;
+            }
+        }
+
+        // since fast->next is the looping point 
+        fast->next = nullptr;
+    }
+}
+
+int main() {
+    Node *head = new Node(50);
+    head->next = new Node(20);
+    head->next->next = new Node(15);
+    head->next->next->next = new Node(4);
+    head->next->next->next->next = new Node(10);
+
+    head->next->next->next->next->next = head;
+
+    detectAndRemoveLoop(head);
+    printList(head);
+
+    return 0;
+}
+
