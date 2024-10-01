bde4988e1879055b6ba184c3be568e9dc09ab9b4
JANANI ASOKAN <chicatechhub@gmail.com>
2024-10-01 15:14:39 +0530

    Create ADV AND DISADV OF STACK.java

---
+++ GRAPHS/ADV AND DISADV OF STACK.java
@@ -0,0 +1,18 @@
+Advantages of Stack Data Structure:
+Simplicity: Stacks are a simple and easy-to-understand data structure, making them suitable for a wide range of applications.
+Efficiency: Push and pop operations on a stack can be performed in constant time (O(1)) , providing efficient access to data.
+Last-in, First-out (LIFO): Stacks follow the LIFO principle, ensuring that the last element added to the stack is the first one removed. This behavior is useful in many scenarios, such as function calls and expression evaluation.
+Limited memory usage: Stacks only need to store the elements that have been pushed onto them, making them memory-efficient compared to other data structures.
+  
+Disadvantages of Stack Data Structure:
+Limited access: Elements in a stack can only be accessed from the top, making it difficult to retrieve or modify elements in the middle of the stack.
+Potential for overflow: If more elements are pushed onto a stack than it can hold, an overflow error will occur, resulting in a loss of data.
+Not suitable for random access: Stack s do not allow for random access to elements, making them unsuitable for applications where elements need to be accessed in a specific order.
+Limited capacity: Stacks have a fixed capacity, which can be a limitation if the number of elements that need to be stored is unknown or highly variable.
+  
+Applications of Stack Data Structure:
+Infix to Postfix /Prefix conversion
+Redo-undo features at many places like editors, photoshop.
+Forward and backward features in web browsers
+In Memory management, any modern computer uses a stack as the primary management for a running purpose. Each program that is running in a computer system has its own memory allocations.
+Stack also helps in implementing function call in computers. The last called function is always completed first.
