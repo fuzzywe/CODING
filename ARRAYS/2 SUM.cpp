748cf69c1780b65774b9c805d0e08c8b6f867b30
JANANI ASOKAN <chicatechhub@gmail.com>
2024-10-20 01:03:29 +0530

    Update 2 SUM.cpp

--- ARRAYS/2 SUM.cpp
+++ ARRAYS/2 SUM.cpp
@@ -120,3 +120,118 @@ Time Complexity: O(N) + O(N*logN), where N = size of the array.
 Reason: The loop will run at most N times. And sorting the array will take N*logN time complexity.
 
 Space Complexity: O(1) as we are not using any extra space.
+Time Complexity: O(N), where N = size of the array.
+Reason: The loop runs N times in the worst case and searching in a hashmap takes O(1) generally. So the time complexity is O(N).
+
+Note: In the worst case(which rarely happens), the unordered_map takes O(N) to find an element. In that case, the time complexity will be O(N2). If we use map instead of unordered_map, the time complexity will be O(N* logN) as the map data structure takes logN time to find an element.
+
+Space Complexity: O(N) as we use the map data structure.
+
+Note: We have optimized this problem enough. But if in the interview, we are not allowed to use the map data structure, then we should move on to the following approach i.e. two pointer approach. This approach will have the same time complexity as the better approach.
+
+Code for Variant 2:
+
+C++
+Java
+#include <bits/stdc++.h>
+using namespace std;
+
+vector<int> twoSum(int n, vector<int> &arr, int target) {
+    unordered_map<int, int> mpp;
+    for (int i = 0; i < n; i++) {
+        int num = arr[i];
+        int moreNeeded = target - num;
+        if (mpp.find(moreNeeded) != mpp.end()) {
+            return {mpp[moreNeeded], i};
+        }
+        mpp[num] = i;
+    }
+    return { -1, -1};
+}
+
+int main()
+{
+    int n = 5;
+    vector<int> arr = {2, 6, 5, 8, 11};
+    int target = 14;
+    vector<int> ans = twoSum(n, arr, target);
+    cout << "This is the answer for variant 2: [" << ans[0] << ", "
+         << ans[1] << "]" << endl;
+    return 0;
+}
+Output: This is the answer for variant 2: [1, 3]
+
+Time Complexity: O(N), where N = size of the array.
+Reason: The loop runs N times in the worst case and searching in a hashmap takes O(1) generally. So the time complexity is O(N).
+
+Note: In the worst case(which rarely happens), the unordered_map takes O(N) to find an element. In that case, the time complexity will be O(N2). If we use map instead of unordered_map, the time complexity will be O(N* logN) as the map data structure takes logN time to find an element.
+
+Space Complexity: O(N) as we use the map data structure.
+
+Note: We have optimized this problem enough. But if in the interview, we are not allowed to use the map data structure, then we should move on to the following approach i.e. two pointer approach. This approach will have the same time complexity as the better approach.
+
+Optimized Approach(using two-pointer): 
+
+Intuition: In this approach, we will first sort the array and will try to choose the numbers in a greedy way.
+
+We will keep a left pointer at the first index and a right pointer at the last index. Now until left < right, we will check the sum of arr[left] and arr[right]. Now if the sum < target, we need bigger numbers and so we will increment the left pointer. But if sum > target, we need to consider lesser numbers and so we will decrement the right pointer. 
+
+If sum == target we will return either “YES” or the indices as per the question.
+But if the left crosses the right pointer, we will return “NO” or {-1, -1}.
+
+Approach:
+
+The steps are the following:
+
+We will sort the given array first.
+Now, we will take two pointers i.e. left, which points to the first index, and right, which points to the last index.
+Now using a loop we will check the sum of arr[left] and arr[right] until left < right.
+If arr[left] + arr[right] > sum, we will decrement the right pointer.
+If arr[left] + arr[right] < sum, we will increment the left pointer.
+If arr[left] + arr[right] == sum, we will return the result.
+Finally, if no results are found we will return “No” or {-1, -1}.
+Dry Run: Given array, nums = [2,1,3,4], target = 4
+
+First, we sort the array. So nums after sorting are [1,2,3,4]
+
+We take two-pointers, left and right. The left points to index 0 and the right points to index 3.
+
+Now we check if nums[left] + nums[right] == target. In this case, they don’t sum up, and nums[left] + nums[right] > target so that we will reduce right by 1.
+
+Now, left = 0, right=2
+
+Here, nums[left] + nums[right] == 1 + 3 == 4, which is the required target, so we will return the result.
+
+Code:
+
+C++
+Java
+#include <bits/stdc++.h>
+using namespace std;
+
+string twoSum(int n, vector<int> &arr, int target) {
+    sort(arr.begin(), arr.end());
+    int left = 0, right = n - 1;
+    while (left < right) {
+        int sum = arr[left] + arr[right];
+        if (sum == target) {
+            return "YES";
+        }
+        else if (sum < target) left++;
+        else right--;
+    }
+    return "NO";
+}
+
+int main()
+{
+    int n = 5;
+    vector<int> arr = {2, 6, 5, 8, 11};
+    int target = 14;
+    string ans = twoSum(n, arr, target);
+    cout << "This is the answer for variant 1: " << ans << endl;
+    return 0;
+}
+Output: This is the answer for variant 1: YES
+
+Note: For variant 2, we can store the elements of the array along with its index in a new array. Then the rest of the code will be similar. And while returning, we need to return the stored indices instead of returning “YES”. But for this variant, the recommended approach is approach 2 i.e. hashing approach.
