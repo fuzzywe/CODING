40021d47da9ff9ac8405749d52103f475275e14d
JANANI ASOKAN <chicatechhub@gmail.com>
2024-10-31 20:19:51 +0530

    Update SEARCH 2D SORTED MATRIX.cpp

--- BINARY SEARCH/SEARCH 2D SORTED MATRIX.cpp
+++ BINARY SEARCH/SEARCH 2D SORTED MATRIX.cpp
@@ -2,6 +2,8 @@ Search in a sorted 2D matrix
 
 https://leetcode.com/problems/search-a-2d-matrix/description/
 
+https://www.naukri.com/code360/problems/search-in-a-2d-matrix_980531?leftPanelTabValue=PROBLEM
+
 
 
 Problem Statement: You have been given a 2-D array 'mat' of size 'N x M' where 'N' and 'M' denote the number of rows and columns, respectively. The elements of each row are sorted in non-decreasing order. Moreover, the first element of a row is greater than the last element of the previous row (if it exists). You are given an integer ‘target’, and your task is to find if it exists in the given 'mat' or not.
@@ -61,41 +63,34 @@ Reason: In order to traverse the matrix, we are using nested loops running for n
 
 Space Complexity: O(1) as we are not using any extra space.
 
-  #include <bits/stdc++.h>
-using namespace std;
-
-bool binarySearch(vector<int>& nums, int target) {
-    int n = nums.size(); //size of the array
-    int low = 0, high = n - 1;
-
-    // Perform the steps:
-    while (low <= high) {
-        int mid = (low + high) / 2;
-        if (nums[mid] == target) return true;
-        else if (target > nums[mid]) low = mid + 1;
-        else high = mid - 1;
-    }
-    return false;
-}
-
-
-bool searchMatrix(vector<vector<int>>& matrix, int target) {
-    int n = matrix.size();
-    int m = matrix[0].size();
-
-    for (int i = 0; i < n; i++) {
-        if (matrix[i][0] <= target && target <= matrix[i][m - 1]) {
-            return binarySearch(matrix[i], target);
-        }
-    }
-    return false;
-}
-
-int main()
+  
+      class Solution {
+public:
+bool binary(vector<int> matrix,int target)
 {
-    vector<vector<int>> matrix = {{1, 2, 3, 4}, {5, 6, 7, 8}, {9, 10, 11, 12}};
-    searchMatrix(matrix, 8) == true ? cout << "true\n" : cout << "false\n";
+    int n = matrix.size();
+        
+        int low = 0;
+        int high = n-1;
+
+        while(low <= high)
+        {
+            int mid = low+(high-low)/2;
+            if(matrix[mid] == target) return true;
+            else if(matrix[mid]>target) high = mid-1;
+            else low = mid+1;
+        }return false;
 }
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        int n =matrix.size();
+        int m = matrix[0].size();
+        for(int i=0;i<matrix.size();i++)
+        {
+            if(matrix[i][0] <= target && matrix[i][m-1]>=target) return binary(matrix[i],target);
+        }
+return false;
+    }
+};
 
 Output: true
 
